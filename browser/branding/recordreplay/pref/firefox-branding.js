1cbee99025ded2f0c4a7ca710aaab28e8cd8262a
Ryan Duffy <ryan.duffy@lge.com>
2021-05-20 20:33:50 -0700

    Hide search engine suggestions from url bar

--- browser/branding/recordreplay/pref/firefox-branding.js
+++ browser/branding/recordreplay/pref/firefox-branding.js
@@ -37,3 +37,9 @@ pref("app.update.badgeWaitTime", 0);
 // Number of usages of the web console.
 // If this is less than 5, then pasting code into the web console is disabled
 pref("devtools.selfxss.count", 5);
+
+// Disable search suggestions
+pref("browser.urlbar.suggest.searches",             false);
+pref("browser.urlbar.suggest.topsites",             false);
+pref("browser.urlbar.suggest.engines",              false);
+pref("browser.urlbar.suggest.quicksuggest",         false);
