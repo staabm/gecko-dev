95352755d48d8165274bd8ed19b563ddc1546367
Kannan Vijayan <kannan@replay.io>
2021-08-27 16:52:12 -0400

    Fix preffing of preallocated recorded browser.

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -807,6 +807,7 @@ pref("toolkit.dump.emit", false);
 // Address of the cloud server to connect to for record/replay sessions.
 pref("devtools.recordreplay.cloudServer", "wss://dispatch.replay.io");
 pref("devtools.recordreplay.recordingsUrl", "https://app.replay.io/");
+pref("devtools.recordreplay.usePreallocated", false);
 pref("devtools.recordreplay.user-token", "");
 
 // Whether to upload source maps and original sources to the cloud server.
