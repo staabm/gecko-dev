d873a561c068e3dae976690c5d11799c2de8cb8d
Brian Hackett <bhackett1024@gmail.com>
2021-10-29 12:12:18 -0600

    Add CI to build/test gecko on pushes

---
+++ .github/actions/build-test/main.js
@@ -0,0 +1,66 @@
+
+const gecko = `${__dirname}/../../..`;
+const revision = getLatestRevision();
+
+sendBuildTestRequest({
+  name: `Gecko Build/Test ${revision}`,
+  tasks: [
+    {
+      id: 0,
+      name: "Build Gecko macOS",
+      task: {
+        kind: "BuildRuntime",
+        runtime: "gecko",
+        revision,
+      },
+      platform: "macOS",
+      dependencies: [],
+    },
+    {
+      id: 1,
+      name: "Run Tests macOS",
+      task: {
+        kind: "StaticLiveTests",
+        runtime: "gecko",
+        revision,
+      },
+      platform: "macOS",
+      dependencies: [0],
+    },
+  ],
+});
+
+function getLatestRevision() {
+  const revision = spawnChecked("git", ["rev-parse", "--short", "HEAD"], { cwd: gecko })
+    .stdout.toString()
+    .trim();
+}
+
+function sendBuildTestRequest(contents) {
+  const text = JSON.stringify(contents);
+
+  const headers {
+    "Content-Type": "application/json",
+    "Content-Length": text.length,
+    Authorization: AuthorizationKey,
+  };
+
+  // Allow overriding the build/test connection info for testing.
+  const options = {
+    hostname: process.env.BUILD_TEST_HOSTNAME || "build-test.replay.io",
+    port: process.env.BUILD_TEST_PORT || 443,
+    path: "/",
+    method: "POST",
+    headers,
+  };
+
+  const request = (process.env.BUILD_TEST_INSECURE ? http : https).request(
+    options,
+    () => {}
+  );
+  request.on("error", e => {
+    console.log(`Error contacting build/test server: ${e}`);
+  });
+  request.write(text);
+  request.end();
+}
