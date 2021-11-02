5e63a86cb2481bd822b4450278b23a7fc6108cc6
Brian Hackett <bhackett1024@gmail.com>
2021-11-02 09:28:55 -0600

    Fill out actions to merge playwright and support releases

--- .github/actions/build-test/main.js
+++ .github/actions/build-test/main.js
@@ -2,96 +2,73 @@ const http = require("http");
 const https = require("https");
 const { spawnSync } = require("child_process");
 
-const gecko = `${__dirname}/../../..`;
-const revision = getLatestRevision();
+const {
+  getLatestReplayRevision,
+  sendBuildTestRequest,
+  spawnChecked,
+  newTask,
+} = require("../utils");
 
-console.log("SendRequest",
-            process.env.BUILD_TEST_AUTHORIZATION.length,
-            process.env.BUILD_TEST_HOSTNAME.length,
-            process.env.BUILD_TEST_PORT.length,
-            process.env.BUILD_TEST_INSECURE.length);
+const gecko = `${__dirname}/../../..`;
+const replayRevision = getLatestReplayRevision();
+const playwrightRevision = getLatestPlaywrightRevision();
 
 sendBuildTestRequest({
-  name: `Gecko Build/Test ${revision}`,
+  name: `Gecko Build/Test ${replayRevision}`,
   tasks: [
-    {
-      id: 0,
-      name: "Build Gecko macOS",
-      task: {
-        kind: "BuildRuntime",
-        runtime: "gecko",
-        revision,
-      },
-      platform: "macOS",
-      dependencies: [],
-    },
-    {
-      id: 1,
-      name: "Run Tests macOS",
-      task: {
-        kind: "StaticLiveTests",
-        runtime: "gecko",
-        revision,
-      },
-      platform: "macOS",
-      dependencies: [0],
-    },
+    ...platformTasks("macOS"),
+    ...platformTasks("linux"),
+    ...platformTasks("windows"),
   ],
 });
 
-function getLatestRevision() {
-  return spawnChecked("git", ["rev-parse", "--short", "HEAD"], { cwd: gecko })
-    .stdout.toString()
-    .trim();
-}
-
-function sendBuildTestRequest(contents) {
-  const text = JSON.stringify(contents);
-
-  const headers = {
-    "Content-Type": "application/json",
-    "Content-Length": text.length,
-    Authorization: process.env.BUILD_TEST_AUTHORIZATION,
-  };
-
-  // Allow overriding the build/test connection info for testing.
-  const options = {
-    hostname: process.env.BUILD_TEST_HOSTNAME || "build-test.replay.io",
-    port: process.env.BUILD_TEST_PORT || 443,
-    path: "/",
-    method: "POST",
-    headers,
-  };
-
-  const request = (process.env.BUILD_TEST_INSECURE ? http : https).request(
-    options,
-    response => {
-      console.log(`RequestFinished Code ${response.statusCode}`);
-      process.exit(response.statusCode == 200 ? 0 : 1);
-    }
+function platformTasks(platform) {
+  const buildReplayTask = newTask(
+    `Build Gecko ${platform}`,
+    {
+      kind: "BuildRuntime",
+      runtime: "gecko",
+      revision: replayRevision,
+    },
+    platform
   );
-  request.on("error", e => {
-    throw new Error(`Error contacting build/test server: ${e}`);
-  });
-  request.write(text);
-  request.end();
 
-  setTimeout(() => {
-    console.log("Timed out waiting for build/test server response");
-    process.exit(1);
-  }, 30000);
-}
+  const testReplayTask = newTask(
+    `Run Tests ${platform}`,
+    {
+      kind: "StaticLiveTests",
+      runtime: "gecko",
+      revision: replayRevision,
+    },
+    platform,
+    [buildReplayTask]
+  );
 
-function spawnChecked(cmd, args, options) {
-  const prettyCmd = [cmd].concat(args).join(" ");
-  console.error(prettyCmd);
+  const buildPlaywrightTask = newTask(
+    `Build Gecko/Playwright ${platform}`,
+    {
+      kind: "BuildRuntime",
+      runtime: "geckoPlaywright",
+      revision: playwrightRevision,
+    },
+    platform
+  );
 
-  const rv = spawnSync(cmd, args, options);
+  const testPlaywrightTask = newTask(
+    `Test Gecko/Playwright ${platform}`,
+    {
+      kind: "PlaywrightLiveTests",
+      runtime: "geckoPlaywright",
+      revision: playwrightRevision,
+    },
+    platform,
+    [buildPlaywrightTask]
+  );
 
-  if (rv.status != 0 || rv.error) {
-    console.error(rv.error);
-    throw new Error(`Spawned process failed with exit code ${rv.status}`);
-  }
-
-  return rv;
+  return [
+    buildReplayTask,
+    testReplayTask,
+    buildPlaywrightTask,
+    testPlaywrightTask
+  ];
 }
